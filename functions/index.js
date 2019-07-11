8512c244ffc9b771dfd6863b08e3cf655cfeee44
João Vanzuita <joaovanzuita@me.com>
2019-07-11 15:34:19 -0300

    adds airbnb and ES6 code style

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,50 @@
-'use strict';
-
+// declarations
 const functions = require('firebase-functions');
-const sh = require("shorthash");
-var topicsArray = ["general", "beauty", "food", "travel", "sports", "entertainment"];
-var modesArray = ["light", "normal", "heavy"];
+const admin = require('firebase-admin');
+const sh = require('shorthash');
+// set CORS
 const cors = require('cors')({
   origin: true,
 });
 
-const admin = require('firebase-admin');
+// set available topics
+const topics = [
+  'general',
+  'beauty',
+  'food',
+  'travel',
+  'sports',
+  'entertainment',
+];
+// set modes
+const modes = ['light', 'normal', 'heavy'];
+// instantiate FireStore
 admin.initializeApp();
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
-exports.getRecentPosts = functions.https.onRequest(async (req, res) => {
+/*
+ * Get Recent Posts
+ */
+module.exports.getRecentPosts = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
-    if (topicsArray.indexOf(req.query.topic) == -1)
-        res.status(403).send("Invalid topic. Allowed topics on this server are : " + topicsArray.join(','));
-    var collRef = db.collection(req.query.topic);
-    collRef.get()
+    if (topics.indexOf(req.query.topic) === -1) {
+      res.status(403).send(
+        `Invalid topic. Allowed topics on this server
+        are : ${topics.join(',')}`,
+      );
+    }
+    const collRef = db.collection(req.query.topic);
+    collRef
+      .get()
       .then((snapshot) => {
-        var array = [];
+        const posts = [];
         snapshot.forEach((doc) => {
           console.log(doc.id, '=>', doc.data());
           console.log('_updateTime:', doc._updateTime._seconds);
-          array.push(doc.data().postid);
+          posts.push(doc.data().postid);
         });
-        res.status(200).send(array);
+        res.status(200).send(posts);
       })
       .catch((err) => {
         console.log('Error getRecentPosts', err);
@@ -35,20 +53,28 @@ exports.getRecentPosts = functions.https.onRequest(async (req, res) => {
   });
 });
 
-exports.getRecentPostsV1 = functions.https.onRequest(async (req, res) => {
+/*
+ * Get Recent Posts V1
+ */
+module.exports.getRecentPostsV1 = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
-    if (topicsArray.indexOf(req.query.topic) == -1)
-        res.status(403).send("Invalid topic. Allowed topics on this server are : " + topicsArray.join(','));
-    var collRef = db.collection(req.query.topic);
-    collRef.get()
+    if (topics.indexOf(req.query.topic) === -1) {
+      res.status(403).send(
+        `Invalid topic. Allowed topics on this server are : 
+        ${topics.join(',')}`,
+      );
+    }
+    const collRef = db.collection(req.query.topic);
+    collRef
+      .get()
       .then((snapshot) => {
-        var array = [];
+        const posts = [];
         snapshot.forEach((doc) => {
           console.log(doc.id, '=>', doc.data());
           console.log('_updateTime:', doc._updateTime._seconds);
-          array.push(doc.data());
+          posts.push(doc.data());
         });
-        res.status(200).send(array);
+        res.status(200).send(posts);
       })
       .catch((err) => {
         console.log('Error getRecentPosts', err);
@@ -57,75 +83,104 @@ exports.getRecentPostsV1 = functions.https.onRequest(async (req, res) => {
   });
 });
 
-exports.publishMyLatestPost = functions.https.onRequest(async (req, res) => {
+/*
+ * Publish Post ID
+ */
+module.exports.publishMyLatestPost = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
-    if (topicsArray.indexOf(req.query.topic) == -1)
-        res.status(403).send("Invalid topic. Allowed topics on this server are : " + topicsArray.join(','));
-    var hashedpostid = sh.unique(req.query.postid);
+    if (topics.indexOf(req.query.topic) === -1) {
+      res.status(403).send(
+        `Invalid topic. Allowed topics on this server are : 
+        ${topics.join(',')}`,
+      );
+    }
+    const hashedpostid = sh.unique(req.query.postid);
     console.log('hashedpostid:', hashedpostid);
     const doctRef = db.collection(req.query.topic).doc(hashedpostid);
 
-    var mode = "normal";
-    if (req.query.mode && modesArray.indexOf(req.query.mode) >= 0) {
+    let mode = 'normal';
+    if (req.query.mode && modes.indexOf(req.query.mode) >= 0) {
       mode = req.query.mode;
     }
-    doctRef.set({
-      'mode': mode,
-      'postid': req.query.postid
-    })
-    .then((snapshot) => {
-      res.status(200).send("hashed:" + hashedpostid + " actual:" + req.query.postid);
-    })
-    .catch((err) => {
-      console.log('Error publishMyLatestPost', err);
-      res.status(403).send(err);
-    });
-  });
-});
-
-exports.deleteOlderPosts = functions.https.onRequest(async (req, res) => {
-  return cors(req, res, () => {
-    if (topicsArray.indexOf(req.query.topic) == -1)
-        res.status(403).send("Invalid topic. Allowed topics on this server are : " + topicsArray.join(','));
-    const hourDeltainSec = 60*60*12
-    const nowinSec = (+ new Date())/1000
-    var collRef = db.collection(req.query.topic)
-    collRef.get().then((snapshot) => {
-      var arr = [];
-      snapshot.forEach((doc) => {
-        if (doc._updateTime._seconds < nowinSec - hourDeltainSec) {
-          console.log('Deleting:', doc.id);
-          doc.ref.delete();
-          arr.push('Deleted:'+ doc.id);
-        } else {
-          console.log('This is recent:', doc.id);
-          arr.push('recent:' + doc.id + " : " + doc._updateTime._seconds);
-        }
+    doctRef
+      .set({
+        mode,
+        postid: req.query.postid,
+      })
+      .then(() => {
+        res
+          .status(200)
+          .send(`hashed:' ${hashedpostid} actual: ${req.query.postid}`);
+      })
+      .catch((err) => {
+        console.log('Error publishMyLatestPost', err);
+        res.status(403).send(err);
       });
-      res.status(200).send(arr);
-    })
-    .catch((err) => {
-      console.log('Error getRecentPosts', err);
-      res.status(403).send(err);
-    });
   });
 });
 
-exports.instapost = functions.https.onRequest(async (req, res) => {
+/*
+ * Delete Old Posts
+ */
+module.exports.deleteOlderPosts = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
-    if (topicsArray.indexOf(req.query.topic) == -1)
-        res.status(403).send("Invalid topic. Allowed topics on this server are : " + topicsArray.join(','));
+    if (topics.indexOf(req.query.topic) === -1) {
+      res.status(403).send(
+        `Invalid topic. Allowed topics on this server are : 
+        ${topics.join(',')}`,
+      );
+    }
+    const hourDeltainSec = 60 * 60 * 12;
+    const nowinSec = +new Date() / 1000;
+    const collRef = db.collection(req.query.topic);
+    collRef
+      .get()
+      .then((snapshot) => {
+        const posts = [];
+        snapshot.forEach((doc) => {
+          if (doc._updateTime._seconds < nowinSec - hourDeltainSec) {
+            console.log('Deleting: ', doc.id);
+            doc.ref.delete();
+            posts.push(`Deleted: ${doc.id}`);
+          } else {
+            console.log('This is recent:', doc.id);
+            posts.push(`recent: doc.id : ${doc._updateTime._seconds}`);
+          }
+        });
+        res.status(200).send(posts);
+      })
+      .catch((err) => {
+        console.log('Error getRecentPosts', err);
+        res.status(403).send(err);
+      });
+  });
+});
+
+/*
+ * InstaPost ?
+ * TODO: improve function name
+ */
+module.exports.instapost = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if (topics.indexOf(req.query.topic) === -1) {
+      res.status(403).send(
+        `Invalid topic. Allowed topics on this server are : 
+        ${topics.join(',')}`,
+      );
+    }
     const doctRef = db.collection(req.query.topic).doc(req.query.hashedpostid);
-    var postid = doctRef.get().then(doc => {
-      if (!doc.exists) {
-        console.log('No such document!');
-      } else {
-        console.log('Document data:', doc.data());
-        res.redirect('https://instagram.com/p/' + doc.data().postid);
-      }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
+    doctRef
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          console.log('Document data:', doc.data());
+          res.redirect(`https://instagram.com/p/' ${doc.data().postid}`);
+        }
+      })
+      .catch((err) => {
+        console.log('Error getting document', err);
+      });
   });
 });
